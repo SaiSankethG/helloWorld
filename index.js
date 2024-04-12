1e149cddce570985560b0e6d8ff5a29c54dfbd0e
Sai <g01sanketh@gmail.com>
2024-04-12 19:37:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log("hello WOrld!")
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
